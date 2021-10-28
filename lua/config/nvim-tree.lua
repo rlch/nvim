b34829aba1628871bab33acbfd77c6bc5984f728
RJM <rjmbowie@gmail.com>
2021-10-28 13:56:55 +1100

    feat: add project.nvim

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,7 +1,9 @@
 local g = vim.g
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
-require'nvim-tree'.setup {
+g.nvim_tree_respect_buf_cwd = 1
+
+require('nvim-tree').setup {
   diagnostics = { enable = false },
   auto_close = false,
   view = {
@@ -9,12 +11,17 @@ require'nvim-tree'.setup {
     mappings = {
       custom_only = false,
       list = {
-        { key = { '<CR>', 'o', '<Tab>' }, cb = tree_cb('edit'), mode = 'n' }
-      }
-    }
+        { key = { '<CR>', 'o' }, cb = tree_cb 'edit', mode = 'n' },
+      },
+    },
   },
-  update_focused_file = { enable = true },
   hijack_cursor = true,
+  update_cwd = false,
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+  },
+  -- update_to_buf_dir = { enable = true },
 }
 -- g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_window_picker_exclude = {
@@ -22,11 +29,10 @@ g.nvim_tree_window_picker_exclude = {
     'notify',
     'packer',
     'qf',
-    'log'
+    'log',
   },
   buftype = {
     'log',
-    '__FLUTTER_DEV_LOG__'
-  }
+    '__FLUTTER_DEV_LOG__',
+  },
 }
-
