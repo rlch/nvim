fd6a3d63efa89b82ca4b06387ee84310b7c894df
RJM <rjmbowie@gmail.com>
2021-10-28 13:57:08 +1100

    feat: imporving autocompletion + flutter support

--- lua/config/flutter-tools.lua
+++ lua/config/flutter-tools.lua
@@ -1,67 +1,67 @@
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		"documentation",
-		"detail",
-		"additionalTextEdits",
-	},
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  },
 }
 
-require("flutter-tools").setup({
-	ui = {
-		-- the border type to use for all floating windows, the same options/formats
-		-- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
-		border = "rounded",
-	},
-	decorations = {
-		statusline = {
-			-- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-			-- this will show the current version of the flutter app from the pubspec.yaml file
-			app_version = false,
-			-- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-			-- this will show the currently running device if an application was started with a specific
-			-- device
-			device = false,
-		},
-	},
-	debugger = { -- integrate with nvim dap + install dart code debugger
-		enabled = true,
-	},
+require('flutter-tools').setup {
+  ui = {
+    -- the border type to use for all floating windows, the same options/formats
+    -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+    border = 'rounded',
+  },
+  decorations = {
+    statusline = {
+      -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+      -- this will show the current version of the flutter app from the pubspec.yaml file
+      app_version = false,
+      -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+      -- this will show the currently running device if an application was started with a specific
+      -- device
+      device = false,
+    },
+  },
+  debugger = { -- integrate with nvim dap + install dart code debugger
+    enabled = true,
+  },
   fvm = true,
-	-- flutter_path = "$HOME/fvm/default/bin/flutter", -- <-- this takes priority over the lookup
-	-- flutter_lookup_cmd = '', -- example "dirname $(which flutter)" or "asdf where flutter"
-	widget_guides = {
-		enabled = false,
-	},
-	closing_tags = {
-		-- highlight = "FlutterClosingTag", -- highlight for the closing tag
-		highlight = "Comment", -- highlight for the closing tag
-		prefix = " ~ ", -- character to use for close tag e.g. > Widget
-		enabled = true, -- set to false to disable
-	},
-	dev_log = {
-		open_cmd = "14new", -- command to use to open the log buffer
-		auto_open = true,
-	},
-	dev_tools = {
-		autostart = false, -- autostart devtools server if not detected
-		auto_open_browser = false, -- Automatically opens devtools in the browser
-	},
-	outline = {
-		open_cmd = "30vnew", -- command to use to open the outline buffer
-		auto_open = false, -- if true this will open the outline automatically when it is first populated
-	},
-	lsp = {
-		-- on_attach = my_custom_on_attach,
-		--- OR you can specify a function to deactivate or change or control how the config is created
-		--[[ capabilities = function(config)
+  -- flutter_path = "$HOME/fvm/default/bin/flutter", -- <-- this takes priority over the lookup
+  -- flutter_lookup_cmd = '', -- example "dirname $(which flutter)" or "asdf where flutter"
+  widget_guides = {
+    enabled = false,
+  },
+  closing_tags = {
+    -- highlight = "FlutterClosingTag", -- highlight for the closing tag
+    highlight = 'Comment', -- highlight for the closing tag
+    prefix = ' ~ ', -- character to use for close tag e.g. > Widget
+    enabled = true, -- set to false to disable
+  },
+  dev_log = {
+    open_cmd = '14new', -- command to use to open the log buffer
+    auto_open = true,
+  },
+  dev_tools = {
+    autostart = false, -- autostart devtools server if not detected
+    auto_open_browser = false, -- Automatically opens devtools in the browser
+  },
+  outline = {
+    open_cmd = '30vnew', -- command to use to open the outline buffer
+    auto_open = false, -- if true this will open the outline automatically when it is first populated
+  },
+  lsp = {
+    -- on_attach = my_custom_on_attach,
+    --- OR you can specify a function to deactivate or change or control how the config is created
+    --[[ capabilities = function(config)
       config.specificThingIDontWant = false
       return config
     end, ]]
-		capabilities = capabilities,
-		on_attach = function(client, bufnr)
-			--[[ require "lsp_signature".setup({
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      --[[ require "lsp_signature".setup({
         bind = true,
         handler_opts = {
           border = "single"
@@ -71,51 +71,48 @@ require("flutter-tools").setup({
         max_width = 100,
         -- use_lspsaga = true,
       }, bufnr) ]]
-		end,
-		settings = {
-			showTodos = true,
-			completeFunctionCalls = true,
-			analysisExcludedFolders = {
-				vim.fn.expand("$HOME/flutter/packages"),
-				vim.fn.expand("$HOME/flutter/bin/cache"),
-				vim.fn.expand("$HOME/flutter/.pub-cache"),
-				vim.fn.expand("$HOME/flutter"),
-				vim.fn.expand("$HOME/.pub-cache"),
-			},
-			lineLength = 120,
-		},
-	},
-})
+    end,
+    settings = {
+      showTodos = false,
+      completeFunctionCalls = true,
+      analysisExcludedFolders = {
+        vim.fn.expand '$HOME/.pub-cache',
+        -- vim.fn.expand("$HOME/fvm/versions"), -- flutter-tools should automatically exclude your SDK.
+      },
+      lineLength = 120,
+    },
+  },
+}
 
-require("telescope").load_extension("flutter")
+require('telescope').load_extension 'flutter'
 
-require("lsp-fastaction").setup({
-	hide_cursor = true,
-	action_data = {
-		--- action for filetype dart
-		["dart"] = {
-			-- pattern is a lua regex with lower case
-			{ pattern = "import library", key = "i", order = 1 },
-			{ pattern = "wrap with widget", key = "w", order = 2 },
-			{ pattern = "wrap with column", key = "c", order = 3 },
-			{ pattern = "wrap with row", key = "r", order = 3 },
-			{ pattern = "wrap with sizedbox", key = "s", order = 3 },
-			{ pattern = "wrap with container", key = "C", order = 4 },
-			{ pattern = "wrap with center", key = "E", order = 4 },
-			{ pattern = "padding", key = "p", order = 4 },
-			{ pattern = "wrap with builder", key = "b", order = 5 },
-			{ pattern = "wrap with streambuilder", key = "S", order = 5 },
-			{ pattern = "remove", key = "d", order = 5 },
+require('lsp-fastaction').setup {
+  hide_cursor = true,
+  action_data = {
+    --- action for filetype dart
+    ['dart'] = {
+      -- pattern is a lua regex with lower case
+      { pattern = 'import library', key = 'i', order = 1 },
+      { pattern = 'wrap with widget', key = 'w', order = 2 },
+      { pattern = 'wrap with column', key = 'c', order = 3 },
+      { pattern = 'wrap with row', key = 'r', order = 3 },
+      { pattern = 'wrap with sizedbox', key = 's', order = 3 },
+      { pattern = 'wrap with container', key = 'C', order = 4 },
+      { pattern = 'wrap with center', key = 'E', order = 4 },
+      { pattern = 'padding', key = 'p', order = 4 },
+      { pattern = 'wrap with builder', key = 'b', order = 5 },
+      { pattern = 'wrap with streambuilder', key = 'S', order = 5 },
+      { pattern = 'remove', key = 'd', order = 5 },
 
-			--range code action
-			{ pattern = "surround with %'if'", key = "i", order = 2 },
-			{ pattern = "try%-catch", key = "t", order = 2 },
-			{ pattern = "for%-in", key = "f", order = 2 },
-			{ pattern = "setstate", key = "s", order = 2 },
-		},
-		["typescript"] = {
-			{ pattern = "to existing import declaration", key = "a", order = 2 },
-			{ pattern = "from module", key = "i", order = 1 },
-		},
-	},
-})
+      --range code action
+      { pattern = "surround with %'if'", key = 'i', order = 2 },
+      { pattern = 'try%-catch', key = 't', order = 2 },
+      { pattern = 'for%-in', key = 'f', order = 2 },
+      { pattern = 'setstate', key = 's', order = 2 },
+    },
+    ['typescript'] = {
+      { pattern = 'to existing import declaration', key = 'a', order = 2 },
+      { pattern = 'from module', key = 'i', order = 1 },
+    },
+  },
+}
--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,36 +1,43 @@
-local cmp = require'cmp'
+local cmp = require 'cmp'
+local lspkind = require 'lspkind'
 local luasnip = require 'luasnip'
 
+local source_mapping = {
+  buffer = '[Buffer]',
+  nvim_lsp = '[LSP]',
+  nvim_lua = '[Lua]',
+  path = '[Path]',
+}
+
 local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+  if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
     return false
   end
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
 end
 
 local feedkey = function(key)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), 'n', true)
 end
 
-
-vim.o.completeopt = "menu,menuone,noselect"
+vim.o.completeopt = 'menu,menuone,noselect'
 
 cmp.setup {
   snippet = {
     expand = function(args)
-      require'luasnip'.lsp_expand(args.body)
-    end
+      require('luasnip').lsp_expand(args.body)
+    end,
   },
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-a>']   = cmp.mapping.complete(),
+    ['<C-a>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<CR>'] = cmp.mapping.confirm { select = true },
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if vim.fn.pumvisible() == 1 then
-        feedkey("<C-n>")
+        feedkey '<C-n>'
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
@@ -38,16 +45,22 @@ cmp.setup {
       else
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    end, {
+      'i',
+      's',
+    }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>")
+        feedkey '<C-p>'
       elseif luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
       end
-    end, { "i", "s" }),
+    end, {
+      'i',
+      's',
+    }),
   },
   sources = {
     { name = 'nvim_lsp' },
@@ -55,62 +68,19 @@ cmp.setup {
     { name = 'neorg' },
     { name = 'buffer' },
     { name = 'path' },
-  }
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      local menu = source_mapping[entry.source.name]
+      --[[ if entry.source.name == 'cmp_tabnine' then
+        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+          menu = entry.completion_item.data.detail .. ' ' .. menu
+        end
+        vim_item.kind = ''
+      end ]]
+      vim_item.menu = menu
+      return vim_item
+    end,
+  },
 }
-
-
-
-
---[[ require('lspconfig')['dartls'].setup {
-} ]]
-
---[[ local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
-
-require'tabout'.setup({
-  tabkey = '',
-  backwards_tabkey = '',
-})
-
-
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-n>'
-  elseif luasnip.expand_or_jumpable() then
-    return t '<Plug>luasnip-expand-or-jump'
-  elseif check_back_space() then
-    return t '<Tab>'
-  else
-    return t '<Plug>(Tabout)'
-  end
-end
-
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-p>'
-  elseif luasnip.jumpable(-1) then
-    return t '<Plug>luasnip-jump-prev'
-  else
-    return t '<Plug>(TaboutBack)'
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})  ]]
